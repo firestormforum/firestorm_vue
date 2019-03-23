1addad57102e26f2fb242e98bbe2359496b20553
Alexander Pulido <alexanderpulido81@hotmail.com>
2019-03-22 23:58:53 -0400

    add eslint config, add @babe/core small fix in the store

--- src/store/AppModule.ts
+++ src/store/AppModule.ts
@@ -1,11 +1,9 @@
 import { Module } from 'vuex';
-import state from './state';
 import getters from './getters';
 import mutations from './mutations';
 import { AppState, RootState } from './types';
 
 export const AppModule: Module<AppState, RootState> = {
-  state,
   getters,
   mutations
 }
